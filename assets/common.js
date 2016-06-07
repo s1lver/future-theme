80592a8a3f655c49d70967ad9b5078ecc4665f87
Evgeniy Moiseenko <s1lverhron@gmail.com>
2016-06-07 16:43:44 +0500

    Added future first template version

--- assets/common.js
+++ assets/common.js
@@ -259,11 +259,6 @@ var trans = {
             this.toX = (e.touches[0].clientX - this.canvas.width / 2) * -0.8;
             this.toY = (e.touches[0].clientY - this.canvas.height / 2) * 0.8;
         }.bind(this));
-        window.addEventListener('mousedown', function(e) {
-            for (var i = 0; i < 100; i++) {
-                this.add();
-            }
-        }.bind(this));
         window.addEventListener('touchstart', function(e) {
             e.preventDefault();
             for (var i = 0; i < 100; i++) {
